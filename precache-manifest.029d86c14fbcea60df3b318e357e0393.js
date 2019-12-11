4f80097255dd24dc9817e50bce1328678566e1b4
vinayb9955 <vinayb9955@gmail.com>
2019-12-11 23:04:43 +0530

    Updates

---
+++ precache-manifest.029d86c14fbcea60df3b318e357e0393.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "323c6677fcd1920f96fe87fbe2408f49",
+    "url": "/phone-directory/index.html"
+  },
+  {
+    "revision": "e9a7d7125004d1d29fdf",
+    "url": "/phone-directory/static/css/main.195c3202.chunk.css"
+  },
+  {
+    "revision": "264ed69fc98d80809fce",
+    "url": "/phone-directory/static/js/2.7fcfd888.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/phone-directory/static/js/2.7fcfd888.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e9a7d7125004d1d29fdf",
+    "url": "/phone-directory/static/js/main.af8628c7.chunk.js"
+  },
+  {
+    "revision": "505cdc9333590f35c409",
+    "url": "/phone-directory/static/js/runtime-main.53d98e78.js"
+  }
+]);
